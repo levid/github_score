c46d9c6111c0027c57c8967e05d052a8c1bbf856
Arthur Chiu <mr.arthur.chiu@gmail.com>
2012-07-26 14:41:48 -0700

    shell out to curl instead

--- lib/github_loader.rb
+++ lib/github_loader.rb
@@ -1,6 +1,6 @@
 class GithubLoader
   def events_for(username)
-    raw_json  = Faraday.get("https://github.com/#{username}.json").body
+    raw_json  = `curl https://github.com/#{username}.json`
     json      = JSON.parse(raw_json)
   end
 end
